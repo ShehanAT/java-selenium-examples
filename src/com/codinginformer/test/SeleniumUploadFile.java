5b9d467a8c0a765dbd0bb2485454f9f9a932423b
Nipun <shehanatuk@gmail.com>
2022-07-06 11:15:39 -0400

    added to SeleniumUploadFile class to com.codinginformer.test

--- src/com/codinginformer/test/SeleniumUploadFile.java
+++ src/com/codinginformer/test/SeleniumUploadFile.java
@@ -3,6 +3,7 @@ package com.codinginformer.test;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,25 +16,26 @@ public class SeleniumUploadFile {
 		 
 		   WebDriver d = new FirefoxDriver();
 		   
-		   d.get("<Insert page containing file upload section>");
+		   d.get("https://ps.uci.edu/~franklin/doc/file_upload.html");
 		   d.manage().window().maximize();
 		   d.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 			
 			
-		   d.manage().window().maximize(); //always write wait code after this
-		   d.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS); //for page load
-		   d.get("https://www.monsterindia.com/seeker/registration"); //Testing webpage
-		   d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //for Implicit wait
+//		   d.manage().window().maximize(); //always write wait code after this
+//		   d.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS); //for page load
+//		   d.get("https://www.monsterindia.com/seeker/registration"); //Testing webpage
+//		   d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //for Implicit wait
 		 
-		   JavascriptExecutor js = (JavascriptExecutor)d; //Scrolling using JavascriptExecutor
-		   js.executeScript("window.scrollBy(0,380)");//Scroll Down to file upload button (+ve)
-		   Thread.sleep(3000);
+//		   JavascriptExecutor js = (JavascriptExecutor); //Scrolling using JavascriptExecutor
+//		   js.executeScript("window.scrollBy(0,380)");//Scroll Down to file upload button (+ve)
+//		   Thread.sleep(3000);
 		 
 		   // FILE UPLOADING USING SENDKEYS ....
 		 
-		   WebElement browse = d.findElement(By.xpath("//input[@id='file-upload']"));
+		   WebElement fileUpload = d.findElement(By.xpath("//input[@value='Send File']"));
 		   //click on ‘Choose file’ to upload the desired file
-		   browse.sendKeys("C:\\Users\\Chait\\Desktop\\Files\\Job Specification.txt"); //Uploading the file using sendKeys
+		   
+		   fileUpload.sendKeys(System.getenv("FILE_UPLOAD_PATH");); //Uploading the file using sendKeys
 		   System.out.println("File is Uploaded Successfully");
 		 
 		   }
