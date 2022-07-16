d3b831feaa3e92366165365b40f6d262d21277d5
Nipun <shehanatuk@gmail.com>
2022-07-16 16:22:58 -0400

    added passing test to com.codinginformer.test.SeleniumGeolocation

--- src/com/codinginformer/test/SeleniumGeolocation.java
+++ src/com/codinginformer/test/SeleniumGeolocation.java
@@ -3,10 +3,14 @@ package com.codinginformer.test;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class SeleniumGeolocation {
 
@@ -20,15 +24,30 @@ public class SeleniumGeolocation {
 	
 	@Test
 	public void checkGeolocation() {
-		String baseUrl = "https://selenium-testing-website.herokuapp.com/upload";
+		String baseUrl = "https://selenium-testing-website.herokuapp.com/geolocation";
 		
-		WebDriver driver = new FirefoxDriver();
+		FirefoxProfile geoDisabled = new FirefoxProfile();
+		geoDisabled.setPreference("geo.enabled", true);
+		geoDisabled.setPreference("geo.provider.use_corelocation", true);
+		geoDisabled.setPreference("geo.prompt.testing", true);
+		geoDisabled.setPreference("geo.prompt.testing.allow", true);
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability(FirefoxDriver.PROFILE, geoDisabled);
+		
+		@SuppressWarnings("deprecation")
+		WebDriver driver = new FirefoxDriver(capabilities);
 
     	driver.get(baseUrl);
     	
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		
+		driver.findElement(By.id("get-geolocation")).click();
+		
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		
+		Assert.assertNotNull(driver.findElement(By.id("lat-value")).getText());
+		Assert.assertNotNull(driver.findElement(By.id("long-value")).getText());
 			
 			
 	}
